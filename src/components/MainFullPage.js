5d353b2ba1fdee729ffa5300d8c6d956d2d5db89
przemo400 <przemyslaw.krysinski@gmail.com>
2023-07-18 22:36:17 +0200

    gatsby-sandbox

---
+++ src/components/MainFullPage.js
@@ -0,0 +1,55 @@
+import * as React from "react";
+
+//import fullpage from 'fullpage.js';
+import ReactFullpage from "@fullpage/react-fullpage";
+import { useRef, useState } from "react";
+import { graphql, useStaticQuery } from "gatsby";
+
+const pluginWrapper = () => {
+    require("../statics/fullpage.scrollHorizontally.min");
+  };
+
+export default function MainFullPage() {
+  //useEffect(() => {setRendered(true)}, []);
+  return (
+    <ReactFullpage
+      //fullpage options
+      licenseKey={"C5L78-EAXO9-8I3O7-0M2EI-YBZMN"}
+      anchors={[
+        "main-entry-video",
+        "main-org-slide",
+        "main-references",
+        "main-partners",
+        "main-contact",
+      ]}
+      sectionsColor={["", "", "#65d4a2", "whitesmoke", "#000"]}
+      menu="#main-menu"
+
+      scrollingSpeed={1000} /* Options here */
+      scrollHorizontally={true} /* Because we are using the extension */
+      scrollHorizontallyKey={"ABFADDBE-C1B24976-B193E3F2-E8FAF606"}
+      controlArrows={false}
+      navigation={true}
+      pluginWrapper={pluginWrapper}
+      render={(comp) => {
+        return (
+          <ReactFullpage.Wrapper>
+            <div className="section">section</div>
+            <div className="section" data-anchor="main-org-slide">
+              <div className="slide bg-cover bg-no-repeat bg-[url('/s1.jpg')] lg:bg-[url('/s1.jpg')]">
+
+              </div>
+              <div className="slide bg-cover bg-no-repeat bg-[url('/s2_mob.jpg')] lg:bg-[url('/s2.jpg')]">
+
+              </div>
+              <div className="slide bg-cover bg-no-repeat bg-[url('/s3_mob.jpg')] lg:bg-[url('/s3.jpg')]">
+    
+              </div>
+            </div>
+            <div className="section">section2</div>
+          </ReactFullpage.Wrapper>
+        );
+      }}
+    />
+  );
+}
